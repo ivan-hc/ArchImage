a51f5acc4d58bf3c20b624939fbdfa9ba4b7731d
iVAN <88724353+ivan-hc@users.noreply.github.com>
2023-06-27 01:22:02 +0200

    Update sample-junest.sh
    Add instructions

--- sample-junest.sh
+++ sample-junest.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
-
+# NAME OF THE APP, REPLACE SAMPLE OR EDIT THE PARTS INCLUDING "$APP" MANUALLY, IF NEEDED
+# FOR EXAMPLE THE PACKAGE "obs-studio" CAN BE STARTED WITH THE BINARY IS "obs"
 APP=SAMPLE
 
-# DOWNLOAD AND INSTALL JUNEST
+# DOWNLOAD AND INSTALL JUNEST (DON'T TOUCH THIS)
 git clone https://github.com/fsquillace/junest.git ~/.local/share/junest
 ./.local/share/junest/bin/junest setup
 
-# INSTALL $APP AND PYTHON
+# INSTALL THE APP WITH ALL THE DEPENDENCES NEEDED, THE WAY YOU DO WITH PACMAN (YOU CAN ALSO REPLACE "$APP", SEE LINE 3)
 ./.local/share/junest/bin/junest -- sudo pacman -Syy
 ./.local/share/junest/bin/junest -- sudo pacman --noconfirm -S $APP
 
-# VERSION NAME
-VERSION=$(./.local/share/junest/bin/junest --  --version | cut -c 14-)
+# VERSION NAME (CHANGE THIS LINE TO OBTAIN THE VERSION FOR YOUR APP)
+VERSION=$(./.local/share/junest/bin/junest -- $APP --version | cut -c 14-) 
 
-# CREATE THE APPDIR
+# CREATE THE APPDIR (DON'T TOUCH THIS)...
 wget -q https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 chmod a+x appimagetool
 mkdir $APP.AppDir
 cp -r ./.local ./$APP.AppDir/
 cp -r ./.junest ./$APP.AppDir/
+
+# ...ADD THE ICON AND THE DESKTOP FILE AT THE ROOT OF THE APPDIR (IF PATHS AND APPS ARE DIFFERENT YOU CAN CHANGE EVERYTHING)...
 cp ./$APP.AppDir/.junest/usr/share/icons/hicolor/scalable/apps/*$APP* ./$APP.AppDir/
 cp ./$APP.AppDir/.junest/usr/share/applications/*$APP* ./$APP.AppDir/
+
+# ...AND FINALLY CREATE THE APPRUN, IE THE MAIN SCRIPT TO RUN THE APPIMAGE!
+# THE APPROACH "echo "$APP $@" | $HERE/.local/share/junest/bin/junest -n" ALLOWS YOU TO RUN THE APP IN A JUNEST SECTION DIRECTLY
+# EDIT THE FOLLOWING LINES IF YOU THINK SOME ENVIRONMENT VARIABLES ARE MISSING
 cat >> ./$APP.AppDir/AppRun << 'EOF'
 #!/bin/sh
 APP=SAMPLE
@@ -32,7 +39,7 @@ echo "$APP $@" | $HERE/.local/share/junest/bin/junest -n
 EOF
 chmod a+x ./$APP.AppDir/AppRun
 
-# REMOVE SOME BLOATWARES
+# REMOVE SOME BLOATWARES (BY DEFAULT I REMOVE /var, YOU CAN ADD WHATEVER YOU WANT IN THIS LIST, THE MORE IMPORTANT THING IS THAT THE APP WORKS) 
 rm -R -f ./$APP.AppDir/.junest/var
 
 # REMOVE THE INBUILT HOME AND SYMLINK THE ONE FROM THE HOST (EXPERIMENTAL, NEEDED FOR PORTABILITY)
