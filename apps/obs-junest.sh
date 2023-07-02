78919400f50d4e8309ff254a4339d8a1c1302006
iVAN <88724353+ivan-hc@users.noreply.github.com>
2023-07-02 18:16:06 +0200

    Update obs-junest.sh

--- apps/obs-junest.sh
+++ apps/obs-junest.sh
@@ -55,6 +55,7 @@ chmod a+x ./$APP.AppDir/AppRun
 # REMOVE "READ-ONLY FILE SYSTEM" ERRORS
 sed -i 's#${JUNEST_HOME}/usr/bin/junest_wrapper#${HOME}/.cache/junest_wrapper.old#g' ./$APP.AppDir/.local/share/junest/lib/core/wrappers.sh
 sed -i 's/rm -f "${JUNEST_HOME}${bin_path}_wrappers/#rm -f "${JUNEST_HOME}${bin_path}_wrappers/g' ./$APP.AppDir/.local/share/junest/lib/core/wrappers.sh
+sed -i 's/ln/#ln/g' ./$APP.AppDir/.local/share/junest/lib/core/wrappers.sh
 
 # REMOVE SOME BLOATWARES
 rm -R -f ./$APP.AppDir/.junest/var
