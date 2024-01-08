151499a030b4566c09b8a623a734002e65fb5888
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-01-08 13:54:49 +0100

    Convert mirrorlist customization into a function

--- sample-next-junest.sh
+++ sample-next-junest.sh
@@ -45,9 +45,12 @@ Include = /etc/pacman.d/mirrorlist" >> ./.junest/etc/pacman.conf
 ###Include = /etc/pacman.d/chaotic-mirrorlist" >> ./.junest/etc/pacman.conf
 
 # CUSTOM MIRRORLIST, THIS SHOULD SPEEDUP THE INSTALLATION OF THE PACKAGES IN PACMAN (COMMENT EVERYTHING TO USE THE DEFAULT MIRROR)
-COUNTRY=$(curl -i ipinfo.io | grep country | cut -c 15- | cut -c -2)
-rm -R ./.junest/etc/pacman.d/mirrorlist
-wget -q https://archlinux.org/mirrorlist/?country="$(echo $COUNTRY)" -O - | sed 's/#Server/Server/g' >> ./.junest/etc/pacman.d/mirrorlist
+_custom_mirrorlist(){
+	COUNTRY=$(curl -i ipinfo.io | grep country | cut -c 15- | cut -c -2)
+	rm -R ./.junest/etc/pacman.d/mirrorlist
+	wget -q https://archlinux.org/mirrorlist/?country="$(echo $COUNTRY)" -O - | sed 's/#Server/Server/g' >> ./.junest/etc/pacman.d/mirrorlist
+}
+#_custom_mirrorlist
 
 # BYPASS SIGNATURE CHECK LEVEL
 sed -i 's/#SigLevel/SigLevel/g' ./.junest/etc/pacman.conf
