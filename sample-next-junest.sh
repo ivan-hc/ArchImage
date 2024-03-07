91bf9e175d7e7a3498a5f7297a7c86012949e6d7
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-03-07 14:26:15 +0100

    Update sample-next-junest.sh: add "pacman-key --populate chaotic"

--- sample-next-junest.sh
+++ sample-next-junest.sh
@@ -32,6 +32,7 @@ Include = /etc/pacman.d/mirrorlist" >> ./.junest/etc/pacman.conf
 # ENABLE CHAOTIC-AUR
 ###./.local/share/junest/bin/junest -- sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 ###./.local/share/junest/bin/junest -- sudo pacman-key --lsign-key 3056513887B78AEB
+###./.local/share/junest/bin/junest -- sudo pacman-key --populate chaotic
 ###./.local/share/junest/bin/junest -- sudo pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 ###echo "
 ###[chaotic-aur]
