fae5cf5e75729e91782c8f7107399096fa68e71d
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-01-02 02:16:25 +0100

    Update sample-next-junest.sh

--- sample-next-junest.sh
+++ sample-next-junest.sh
@@ -204,29 +204,27 @@ function _create_AppRun() {
 
 	[ -z "$NVIDIA_ON" ] && NVIDIA_ON=0
 	if [ "$NVIDIA_ON" = 1 ]; then
-	  DATADIR="${XDG_DATA_HOME:-$HOME/.local/share}"
-	  CONTY_DIR="${DATADIR}/Conty/overlayfs_shared"
-	  CACHEDIR="${XDG_CACHE_HOME:-$HOME/.cache}"
-	  [ -f /sys/module/nvidia/version ] && nvidia_driver_version="$(cat /sys/module/nvidia/version)"
-	  [ -f "${CONTY_DIR}"/nvidia/current-nvidia-version ] && nvidia_driver_conty="$(cat "${CONTY_DIR}"/nvidia/current-nvidia-version)"
-	  if [ "${nvidia_driver_version}" != "${nvidia_driver_conty}" ]; then
-	     if command -v curl >/dev/null 2>&1; then
-	        if ! curl --output /dev/null --silent --head --fail https://github.com 1>/dev/null; then
-	          notify-send "You are offline, cannot use Nvidia drivers"
-	        else
-	          notify-send "Configuring Nvidia drivers for this AppImage..."
-	          mkdir -p "${CACHEDIR}" && cd "${CACHEDIR}" || exit 1
-	          curl -Ls "https://raw.githubusercontent.com/ivan-hc/ArchImage/main/nvidia-junest.sh" > nvidia-junest.sh
-	          chmod a+x ./nvidia-junest.sh && ./nvidia-junest.sh
-	        fi
-	     else
-	        notify-send "Missing \"curl\" command, cannot use Nvidia drivers"
-	        echo "You need \"curl\" to download this script"
-	     fi
-	  fi
-	  [ -d "${CONTY_DIR}"/up/usr/bin ] && export PATH="${PATH}":"${CONTY_DIR}"/up/usr/bin:"${PATH}"
-	  [ -d "${CONTY_DIR}"/up/usr/lib ] && export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}":"${CONTY_DIR}"/up/usr/lib:"${LD_LIBRARY_PATH}"
-	  [ -d "${CONTY_DIR}"/up/usr/share ] && export XDG_DATA_DIRS="${XDG_DATA_DIRS}":"${CONTY_DIR}"/up/usr/share:"${XDG_DATA_DIRS}"
+	   DATADIR="${XDG_DATA_HOME:-$HOME/.local/share}"
+	   CONTY_DIR="${DATADIR}/Conty/overlayfs_shared"
+	   [ -f /sys/module/nvidia/version ] && nvidia_driver_version="$(cat /sys/module/nvidia/version)"
+	   if [ -n "$nvidia_driver_version" ]; then
+	      mkdir -p "${CONTY_DIR}"/nvidia "${CONTY_DIR}"/up/usr/lib "${CONTY_DIR}"/up/usr/share
+	      nvidia_data_dirs="egl glvnd nvidia vulkan"
+	      for d in $nvidia_data_dirs; do [ ! -d "${CONTY_DIR}"/up/usr/share/"$d" ] && ln -s /usr/share/"$d" "${CONTY_DIR}"/up/usr/share/ 2>/dev/null; done
+	      [ ! -f "${CONTY_DIR}"/nvidia/current-nvidia-version ] && echo "${nvidia_driver_version}" > "${CONTY_DIR}"/nvidia/current-nvidia-version
+	      [ -f "${CONTY_DIR}"/nvidia/current-nvidia-version ] && nvidia_driver_conty=$(cat "${CONTY_DIR}"/nvidia/current-nvidia-version)
+	      if [ "${nvidia_driver_version}" != "${nvidia_driver_conty}" ]; then
+	         rm -f "${CONTY_DIR}"/up/usr/lib/*; echo "${nvidia_driver_version}" > "${CONTY_DIR}"/nvidia/current-nvidia-version
+	      fi
+	      /sbin/ldconfig -p > "${CONTY_DIR}"/nvidia/host_libs
+	      grep -i "nvidia\|libcuda" "${CONTY_DIR}"/nvidia/host_libs | cut -d ">" -f 2 > "${CONTY_DIR}"/nvidia/host_nvidia_libs
+	      libnv_paths=$(grep "libnv" "${CONTY_DIR}"/nvidia/host_libs | cut -d ">" -f 2)
+	      for f in $libnv_paths; do strings "${f}" | grep -qi -m 1 "nvidia" && echo "${f}" >> "${CONTY_DIR}"/nvidia/host_nvidia_libs; done
+	      nvidia_libs=$(cat "${CONTY_DIR}"/nvidia/host_nvidia_libs)
+	      for n in $nvidia_libs; do libname=$(echo "$n" | sed 's:.*/::') && [ ! -f "${CONTY_DIR}"/up/usr/lib/"$libname" ] && cp "$n" "${CONTY_DIR}"/up/usr/lib/; done
+	      [ -d "${CONTY_DIR}"/up/usr/lib ] && export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}":"${CONTY_DIR}"/up/usr/lib:"${LD_LIBRARY_PATH}"
+	      [ -d "${CONTY_DIR}"/up/usr/share ] && export XDG_DATA_DIRS="${XDG_DATA_DIRS}":"${CONTY_DIR}"/up/usr/share:"${XDG_DATA_DIRS}"
+	   fi
 	fi
 
 	BINDS=" --dev-bind /dev /dev \
@@ -558,4 +556,4 @@ if test -f ./*.AppImage; then
 	rm -R -f ./*archimage*.AppImage
 fi
 ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 ./$APP.AppDir
-mv ./*AppImage ./"$(cat ./"$APP".AppDir/*.desktop | grep 'Name=' | head -1 | cut -c 6- | sed 's/ /-/g')"_"$VERSION"-archimage4-x86_64.AppImage
+mv ./*AppImage ./"$(cat ./"$APP".AppDir/*.desktop | grep 'Name=' | head -1 | cut -c 6- | sed 's/ /-/g')"_"$VERSION"-archimage4.1-x86_64.AppImage
