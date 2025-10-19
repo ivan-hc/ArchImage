de0f414326c2ac88d8d6d0f7e1b24eb6a915261e
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-10-19 14:21:45 +0200

    Add NEW-junest-anylinux.sh (experimental)

---
+++ NEW-junest-anylinux.sh
@@ -0,0 +1,167 @@
+#!/usr/bin/env bash
+
+##########################################################################################################################################################
+#	USER'S SETTINGS
+##########################################################################################################################################################
+
+# Set the main package name for Arch Linux (APP), the binary name (BIN) and the dependencies (DEPENDENCES).
+APP=SAMPLE
+BIN="$APP" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
+DEPENDENCES="" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
+#BASICSTUFF="binutils debugedit gzip"
+#COMPILERS="base-devel"
+
+##########################################################################################################################################################
+#	SETUP THE ENVIRONMENT
+##########################################################################################################################################################
+
+# Create and enter the AppDir
+mkdir -p archlinux && cd archlinux || exit 1
+
+_JUNEST_CMD() {
+	./.local/share/junest/bin/junest "$@"
+}
+
+# Set archlinux as a temporary $HOME directory
+HOME="$(dirname "$(readlink -f "$0")")"
+
+##########################################################################################################################################################
+#	DOWNLOAD, INSTALL AND CONFIGURE JUNEST
+##########################################################################################################################################################
+
+_enable_multilib() {
+	printf "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> ./.junest/etc/pacman.conf
+}
+
+_enable_chaoticaur() {
+	# This function is ment to be used during the installation of JuNest, see "_pacman_patches"
+	_JUNEST_CMD -- sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+	_JUNEST_CMD -- sudo pacman-key --lsign-key 3056513887B78AEB
+	_JUNEST_CMD -- sudo pacman-key --populate chaotic
+	_JUNEST_CMD -- sudo pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+	printf "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> ./.junest/etc/pacman.conf
+}
+
+_enable_archlinuxcn() {
+	_JUNEST_CMD -- sudo pacman --noconfirm -U "https://repo.archlinuxcn.org/x86_64/$(curl -Ls https://repo.archlinuxcn.org/x86_64/ | tr '"' '\n' | grep "^archlinuxcn-keyring.*zst$" | tail -1)"
+	printf "\n[archlinuxcn]\n#SigLevel = Never\nServer = http://repo.archlinuxcn.org/\$arch" >> ./.junest/etc/pacman.conf
+}
+
+_custom_mirrorlist() {
+	COUNTRY=$(curl -i ipinfo.io 2>/dev/null | grep country | cut -c 15- | cut -c -2)
+	if [ -n "$GITHUB_REPOSITORY_OWNER" ] || ! curl --output /dev/null --silent --head --fail "https://archlinux.org/mirrorlist/?country=$COUNTRY" 1>/dev/null; then
+		curl -Ls https://archlinux.org/mirrorlist/all | awk NR==2 RS= | sed 's/#Server/Server/g' > ./.junest/etc/pacman.d/mirrorlist
+	else
+		curl -Ls "https://archlinux.org/mirrorlist/?country=$COUNTRY" | sed 's/#Server/Server/g' > ./.junest/etc/pacman.d/mirrorlist
+	fi
+}
+
+_bypass_signature_check_level() {
+	sed -i 's/#SigLevel/SigLevel/g; s/Required DatabaseOptional/Never/g' ./.junest/etc/pacman.conf
+}
+
+_install_junest() {
+	printf -- "-----------------------------------------------------------------------------\n◆ Clone JuNest from https://github.com/fsquillace/junest\n-----------------------------------------------------------------------------\n"
+	git clone https://github.com/fsquillace/junest.git ./.local/share/junest
+	printf -- "-----------------------------------------------------------------------------\n◆ Downloading JuNest archive from https://github.com/ivan-hc/junest\n-----------------------------------------------------------------------------\n"
+	curl -#Lo junest-x86_64.tar.gz https://github.com/ivan-hc/junest/releases/download/continuous/junest-x86_64.tar.gz
+	_JUNEST_CMD setup -i junest-x86_64.tar.gz
+	rm -f junest-x86_64.tar.gz
+	echo " Apply patches to PacMan..."
+	#_enable_multilib
+	#_enable_chaoticaur
+	#_enable_archlinuxcn
+	_custom_mirrorlist
+	_bypass_signature_check_level
+
+	# Update arch linux in junest
+	_JUNEST_CMD -- sudo pacman -Syy
+	_JUNEST_CMD -- sudo pacman --noconfirm -Syu
+}
+
+if ! test -d "$HOME/.local/share/junest"; then
+	printf -- "-----------------------------------------------------------------------------\n DOWNLOAD, INSTALL AND CONFIGURE JUNEST\n-----------------------------------------------------------------------------\n"
+	_install_junest
+else
+	printf -- "-----------------------------------------------------------------------------\n RESTART JUNEST\n-----------------------------------------------------------------------------\n"
+fi
+
+##########################################################################################################################################################
+#	INSTALL PROGRAMS USING YAY
+##########################################################################################################################################################
+
+_JUNEST_CMD -- yay -Syy
+#_JUNEST_CMD -- gpg --keyserver keyserver.ubuntu.com --recv-key C01E1CAD5EA2C4F0B8E3571504C367C218ADD4FF # UNCOMMENT IF YOU USE THE AUR
+if [ -n "$BASICSTUFF" ]; then
+	_JUNEST_CMD -- yay --noconfirm -S $BASICSTUFF
+fi
+if [ -n "$COMPILERS" ]; then
+	_JUNEST_CMD -- yay --noconfirm -S $COMPILERS
+	_JUNEST_CMD -- yay --noconfirm -S python # to force one Python version and prevent modules from being installed in different directories (e.g. "mesonbuild")
+fi
+if [ -n "$DEPENDENCES" ]; then
+	_JUNEST_CMD -- yay --noconfirm -S $DEPENDENCES
+fi
+if [ -n "$APP" ]; then
+	_JUNEST_CMD -- yay --noconfirm -S xorg-server-xvfb zsync
+	_JUNEST_CMD -- yay --noconfirm -S "$APP"
+	_JUNEST_CMD -- glib-compile-schemas /usr/share/glib-2.0/schemas/
+else
+	echo "No app found, exiting"; exit 1
+fi
+
+##########################################################################################################################################################
+#	CREATE THE APPIMAGE
+##########################################################################################################################################################
+
+# AppDir setup
+mkdir -p AppDir
+
+# Add launcher and icon
+rm -f ./*.desktop
+LAUNCHER=$(grep -iRl "$BIN" .junest/usr/share/applications/* | grep ".desktop" | head -1)
+cp -r "$LAUNCHER" AppDir/
+ICON=$(cat "$LAUNCHER" | grep "Icon=" | cut -c 6-)
+[ -z "$ICON" ] && ICON="$BIN"
+cp -r .junest/usr/share/icons/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/22x22/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/24x24/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/32x32/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/48x48/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/64x64/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/128x128/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/192x192/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/256x256/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/512x512/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/icons/hicolor/scalable/apps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+cp -r .junest/usr/share/pixmaps/*"$ICON"* "$APP".AppDir/ 2>/dev/null
+
+# Version
+export VERSION="$(_JUNEST_CMD -- yay -Q "$APP" | awk '{print $2; exit}')"
+echo "$VERSION" > ~/version
+
+# Anylinux variables
+ARCH="x86_64"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+export ADD_HOOKS="self-updater.bg.hook"
+export APPNAME=$(cat AppDir/*.desktop | grep 'Name=' | head -1 | cut -c 6- | sed 's/ /-/g')
+export REPO="$APPNAME-appimage"
+export TAG="latest"
+export UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|$REPO|$TAG|*x86_64.AppImage.zsync"
+
+export OUTNAME="$APPNAME"-"$VERSION"-anylinux+archimage5-"$ARCH".AppImage
+
+# Deploy dependencies
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+_JUNEST_CMD -- ./quick-sharun /usr/bin/"$BIN"
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+_JUNEST_CMD -- ./uruntime2appimage
+
+cd ..
+mv archlinux/*.AppImage* ./
