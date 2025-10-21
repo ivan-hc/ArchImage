9e4990c61f6cfaa717238a39e60d358b3b76fbbf
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-10-21 18:43:50 +0200

    Refactoring of the AppRun

--- NEW-junest.sh
+++ NEW-junest.sh
@@ -212,74 +212,58 @@ cat <<-'HEREDOC' >> AppDir/AppRun
 HERE="$(dirname "$(readlink -f "$0")")"
 export JUNEST_HOME="$HERE"/.junest
 
+CACHEDIR="${XDG_CACHE_HOME:-$HOME/.cache}"
+mkdir -p "$CACHEDIR" || exit 1
+
 if command -v unshare >/dev/null 2>&1 && ! unshare --user -p /bin/true >/dev/null 2>&1; then
-   PROOT_ON=1
-   export PATH="$HERE"/.local/share/junest/bin/:"$PATH"
-   mkdir -p "$HOME"/.cache
+   PROOT_ON=1 && export PATH="$HERE"/.local/share/junest/bin/:"$PATH"
 else
    export PATH="$PATH":"$HERE"/.local/share/junest/bin
 fi
 
-[ -z "$NVIDIA_ON" ] && NVIDIA_ON=0
-if [ "$NVIDIA_ON" = 1 ]; then
-   DATADIR="${XDG_DATA_HOME:-$HOME/.local/share}"
-   CONTY_DIR="${DATADIR}/Conty/overlayfs_shared"
-   [ -f /sys/module/nvidia/version ] && nvidia_driver_version="$(cat /sys/module/nvidia/version)"
-   if [ -n "$nvidia_driver_version" ]; then
-      mkdir -p "${CONTY_DIR}"/nvidia "${CONTY_DIR}"/up/usr/lib
-      [ ! -f "${CONTY_DIR}"/nvidia/current-nvidia-version ] && echo "${nvidia_driver_version}" > "${CONTY_DIR}"/nvidia/current-nvidia-version
-      [ -f "${CONTY_DIR}"/nvidia/current-nvidia-version ] && nvidia_driver_conty=$(cat "${CONTY_DIR}"/nvidia/current-nvidia-version)
-      if [ "${nvidia_driver_version}" != "${nvidia_driver_conty}" ]; then
-         rm -f "${CONTY_DIR}"/up/usr/lib/*; echo "${nvidia_driver_version}" > "${CONTY_DIR}"/nvidia/current-nvidia-version
-      fi
-      /sbin/ldconfig -p > "${CONTY_DIR}"/nvidia/host_libs
-      grep -i "nvidia\|libcuda" "${CONTY_DIR}"/nvidia/host_libs | cut -d ">" -f 2 > "${CONTY_DIR}"/nvidia/host_nvidia_libs
-      libnv_paths=$(grep "libnv" "${CONTY_DIR}"/nvidia/host_libs | cut -d ">" -f 2)
-      for f in $libnv_paths; do
-         strings "${f}" | grep -qi -m 1 "nvidia" && echo "${f}" >> "${CONTY_DIR}"/nvidia/host_nvidia_libs
-      done
-      nvidia_libs=$(cat "${CONTY_DIR}"/nvidia/host_nvidia_libs)
-      for n in $nvidia_libs; do
-         libname=$(echo "$n" | sed 's:.*/::') && [ ! -f "${CONTY_DIR}"/up/usr/lib/"$libname" ] && cp "$n" "${CONTY_DIR}"/up/usr/lib/
-      done
-      libvdpau_nvidia="${CONTY_DIR}/up/usr/lib/libvdpau_nvidia.so"
-      if ! test -f "${libvdpau_nvidia}*"; then
-         cp "$(find /usr/lib -type f -name 'libvdpau_nvidia.so*' -print -quit 2>/dev/null | head -1)" "${CONTY_DIR}"/up/usr/lib/
-      fi
-      [ -f "${libvdpau_nvidia}"."${nvidia_driver_version}" ] && [ ! -f "${libvdpau_nvidia}" ] && ln -s "${libvdpau_nvidia}"."${nvidia_driver_version}" "${libvdpau_nvidia}"
-      [ -d "${CONTY_DIR}"/up/usr/lib ] && export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}":"${CONTY_DIR}"/up/usr/lib:"${LD_LIBRARY_PATH}"
+[ -z "$NVIDIA_ON" ] && NVIDIA_ON=1
+if [ -f /sys/module/nvidia/version ] && [ "$NVIDIA_ON" = 1 ]; then
+   nvidia_driver_version="$(cat /sys/module/nvidia/version)"
+   JUNEST_DIRS="${CACHEDIR}/junest_shared/usr" JUNEST_LIBS="${JUNEST_DIRS}/lib" JUNEST_NVIDIA_DATA="${JUNEST_DIRS}/share/nvidia"
+   mkdir -p "${JUNEST_LIBS}" "${JUNEST_NVIDIA_DATA}" || exit 1
+   [ ! -f "${JUNEST_NVIDIA_DATA}"/current-nvidia-version ] && echo "${nvidia_driver_version}" > "${JUNEST_NVIDIA_DATA}"/current-nvidia-version
+   [ -f "${JUNEST_NVIDIA_DATA}"/current-nvidia-version ] && nvidia_driver_conty=$(cat "${JUNEST_NVIDIA_DATA}"/current-nvidia-version)
+   if [ "${nvidia_driver_version}" != "${nvidia_driver_conty}" ]; then
+      rm -f "${JUNEST_LIBS}"/*; echo "${nvidia_driver_version}" > "${JUNEST_NVIDIA_DATA}"/current-nvidia-version
    fi
+   HOST_LIBS=$(/sbin/ldconfig -p)
+   libnvidia_libs=$(echo "$HOST_LIBS" | grep -i "nvidia\|libcuda" | cut -d ">" -f 2)
+   libvdpau_nvidia=$(find /usr/lib -type f -name 'libvdpau_nvidia.so*' -print -quit 2>/dev/null | head -1)
+   libnv_paths=$(echo "$HOST_LIBS" | grep "libnv" | cut -d ">" -f 2)
+   for f in $libnv_paths; do strings "${f}" | grep -qi -m 1 "nvidia" && libnv_libs="$libnv_libs ${f}"; done
+   host_nvidia_libs=$(echo "$libnv_libs $libnvidia_libs $libvdpau_nvidia" | sed 's/ /\n/g' | sort | grep .)
+   for n in $host_nvidia_libs; do libname=$(echo "$n" | sed 's:.*/::') && [ ! -f "${JUNEST_LIBS}"/"$libname" ] && cp "$n" "${JUNEST_LIBS}"/; done
+   libvdpau="${JUNEST_LIBS}/libvdpau_nvidia.so"
+   [ -f "${libvdpau}"."${nvidia_driver_version}" ] && [ ! -f "${libvdpau}" ] && ln -s "${libvdpau}"."${nvidia_driver_version}" "${libvdpau}"
+   export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}":"${JUNEST_LIBS}":"${LD_LIBRARY_PATH}"
 fi
 
-PROOT_BINDINGS=""
-BWRAP_BINDINGS=""
-
 bind_files="/etc/resolv.conf /etc/hosts /etc/nsswitch.conf /etc/passwd /etc/group /etc/machine-id /etc/asound.conf /etc/localtime "
-for f in $bind_files; do
-   [ -f "$f" ] && PROOT_BINDINGS=" $PROOT_BINDINGS --bind=$f" && BWRAP_BINDINGS=" $BWRAP_BINDINGS --ro-bind-try $f $f"
-done
-
 bind_nvidia_data_dirs="/usr/share/egl /usr/share/glvnd /usr/share/nvidia /usr/share/vulkan"
 bind_dirs=" /media /mnt /opt /run/media /usr/lib/locale /usr/share/fonts /usr/share/themes /var $bind_nvidia_data_dirs"
-for d in $bind_dirs; do
-   [ -d "$d" ] && PROOT_BINDINGS=" $PROOT_BINDINGS --bind=$d" && BWRAP_BINDINGS=" $BWRAP_BINDINGS --bind-try $d $d"
-done
-
-PROOT_BINDS=" --bind=/dev --bind=/sys --bind=/tmp --bind=/proc $PROOT_BINDINGS --bind=/home --bind=/home/$USER "
-BWRAP_BINDS=" --dev-bind /dev /dev --ro-bind /sys /sys --bind-try /tmp /tmp --proc /proc $BWRAP_BINDINGS --cap-add CAP_SYS_ADMIN "
+if [ "$PROOT_ON" = 1 ]; then
+   for f in $bind_files; do [ -f "$f" ] && BINDINGS=" $BINDINGS --bind=$f"; done
+   for d in $bind_dirs; do [ -d "$d" ] && BINDINGS=" $BINDINGS --bind=$d"; done
+   junest_options="proot -n -b"
+   junest_bindings=" --bind=/dev --bind=/sys --bind=/tmp --bind=/proc $BINDINGS --bind=/home --bind=/home/$USER "
+else
+   for f in $bind_files; do [ -f "$f" ] && BINDINGS=" $BINDINGS --ro-bind-try $f $f"; done
+   for d in $bind_dirs; do [ -d "$d" ] && BINDINGS=" $BINDINGS --bind-try $d $d"; done
+   junest_options="-n -b"
+   junest_bindings=" --dev-bind /dev /dev --ro-bind /sys /sys --bind-try /tmp /tmp --proc /proc $BINDINGS --cap-add CAP_SYS_ADMIN "
+fi
 
 _JUNEST_CMD() {
-   if [ "$PROOT_ON" = 1 ]; then
-      "$HERE"/.local/share/junest/bin/junest proot -n -b "$PROOT_BINDS" "$@"
-   else
-      "$HERE"/.local/share/junest/bin/junest -n -b "$BWRAP_BINDS" "$@"
-   fi
+   "$HERE"/.local/share/junest/bin/junest $junest_options "$junest_bindings" "$@"
 }
 
 EXEC=$(grep -e '^Exec=.*' "${HERE}"/*.desktop | head -n 1 | cut -d "=" -f 2- | sed -e 's|%.||g')
-if ! echo "$EXEC" | grep -q "/usr/bin"; then
-   EXEC="/usr/bin/$EXEC"
-fi
+if ! echo "$EXEC" | grep -q "/usr/bin"; then EXEC="/usr/bin/$EXEC"; fi
 
 _JUNEST_CMD -- $EXEC "$@"
 
