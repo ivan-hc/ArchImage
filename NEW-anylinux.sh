f170124f6f2b3925731e75a8ef881a18736fb2f8
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-10-22 15:59:55 +0200

    Update NEW-anylinux.sh - support keywords check

--- NEW-anylinux.sh
+++ NEW-anylinux.sh
@@ -6,6 +6,27 @@ DEPENDENCES="" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPEND
 #BASICSTUFF="binutils debugedit gzip"
 #COMPILERS="base-devel"
 
+# Set keywords to searchan include in names of directories and files in /usr/bin (BINSAVED), /usr/share (SHARESAVED) and /usr/lib (LIBSAVED)
+BINSAVED="SAVEBINSPLEASE"
+SHARESAVED="SAVESHAREPLEASE"
+LIBSAVED="SAVELIBSPLEASE"
+
+# Set the items you want to manually REMOVE. Complete the path in /etc/, /usr/bin/, /usr/lib/, /usr/lib/python*/ and /usr/share/ respectively.
+# The "rm" command will take into account the listed object/path and add an asterisk at the end, completing the path to be removed.
+# Some keywords and paths are already set. Remove them if you consider them necessary for the AppImage to function properly.
+ETC_REMOVED="makepkg.conf pacman"
+BIN_REMOVED="gcc"
+LIB_REMOVED="gcc"
+PYTHON_REMOVED="__pycache__/"
+SHARE_REMOVED="gcc"
+
+# Post-installation processes (add whatever you want)
+_post_installation_processes() {
+	printf "\n◆ User's processes: \n\n"
+	echo " - None"
+	# Add here your code
+}
+
 ##########################################################################################################################################################
 #	SETUP THE ENVIRONMENT
 ##########################################################################################################################################################
@@ -101,10 +122,19 @@ fi
 if [ -n "$APP" ]; then
 	_JUNEST_CMD -- yay --noconfirm -S alsa-lib gtk3 hicolor-icon-theme xapp xdg-utils xorg-server-xvfb zsync
 	_JUNEST_CMD -- yay --noconfirm -S "$APP"
-	EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
-	wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-	chmod +x ./get-debloated-pkgs.sh
-	_JUNEST_CMD -- ./get-debloated-pkgs.sh --add-common
+	# Use debloated packages
+	debloated_soueces="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous"
+	extra_vk_packages="vulkan-asahi vulkan-broadcom vulkan-freedreno vulkan-intel vulkan-nouveau vulkan-panfrost vulkan-radeon"
+	extra_packages="ffmpeg gdk-pixbuf2 gtk3 gtk4 intel-media-driver llvm-libs mangohud mesa opus qt6-base $extra_vk_packages"
+	for p in $extra_packages; do
+		if _JUNEST_CMD -- yay -Qs "$p"; then
+			if [ ! -f ./"$p"-2.x-x86_64.pkg.tar.zst ]; then
+				curl -#Lo "$p"-2.x-x86_64.pkg.tar.zst "$debloated_soueces/$p-mini-x86_64.pkg.tar.zst" || exit 1
+			fi
+			_JUNEST_CMD -- yay --noconfirm -U "$HOME"/"$p"-2.x-x86_64.pkg.tar.zst
+		fi
+	done
+	# Try to compile schema files
 	_JUNEST_CMD -- glib-compile-schemas /usr/share/glib-2.0/schemas/
 else
 	echo "No app found, exiting"; exit 1
@@ -157,7 +187,95 @@ wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 _JUNEST_CMD -- ./quick-sharun /usr/bin/"$BIN"
 
-# MAKE APPIMAGE WITH URUNTIME
+printf -- "\n-----------------------------------------------------------------------------\n IMPLEMENTING USER'S SELECTED FILES AND DIRECTORIES\n-----------------------------------------------------------------------------\n\n"
+
+# Save files in /usr/bin
+_savebins() {
+	echo "◆ Saving files in /usr/bin"
+	for arg in $BINSAVED; do
+		cp -r ./.junest/usr/bin/*"$arg"* AppDir/bin/
+	done
+}
+
+# Save files in /usr/lib
+_savelibs() {
+	echo "◆ Detect libraries related to /usr/bin files"
+	libs4bin=$(readelf -d AppDir/bin/* 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so")
+
+	LIBSAVED="$libs4bin $LIBSAVED"
+	for arg in $LIBSAVED; do
+		LIBPATHS="$LIBPATHS $(find ./.junest/usr/lib -maxdepth 20 -wholename "*$arg*" | sed 's/\.\/archlinux\///g')"
+	done
+	for arg in $LIBPATHS; do
+		[ ! -d AppDir/"$arg" ] && cp -r ./archlinux/"$arg" AppDir/"$arg" &
+	done
+	wait
+	core_libs=$(find AppDir -type f)
+	lib_core=$(for c in $core_libs; do readelf -d "$c" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+
+	echo "◆ Detect and copy base libs"
+	basebin_libs=$(find ./AppDir -executable -name "*.so*")
+	lib_base_1=$(for b in $basebin_libs; do readelf -d "$b" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+	lib_base_1=$(echo "$lib_base_1" | tr ' ' '\n' | sort -u | xargs)
+	lib_base_2=$(for b in $lib_base_1; do readelf -d ./.junest/usr/lib/"$b" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+	lib_base_2=$(echo "$lib_base_2" | tr ' ' '\n' | sort -u | xargs)
+	lib_base_3=$(for b in $lib_base_2; do readelf -d ./.junest/usr/lib/"$b" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+	lib_base_3=$(echo "$lib_base_3" | tr ' ' '\n' | sort -u | xargs)
+	lib_base_4=$(for b in $lib_base_3; do readelf -d ./.junest/usr/lib/"$b" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+	lib_base_4=$(echo "$lib_base_4" | tr ' ' '\n' | sort -u | xargs)
+	lib_base_5=$(for b in $lib_base_4; do readelf -d ./.junest/usr/lib/"$b" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+	lib_base_5=$(echo "$lib_base_5" | tr ' ' '\n' | sort -u | xargs)
+	lib_base_6=$(for b in $lib_base_5; do readelf -d ./.junest/usr/lib/"$b" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+	lib_base_6=$(echo "$lib_base_6" | tr ' ' '\n' | sort -u | xargs)
+	lib_base_7=$(for b in $lib_base_6; do readelf -d ./.junest/usr/lib/"$b" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+	lib_base_7=$(echo "$lib_base_7" | tr ' ' '\n' | sort -u | xargs)
+	lib_base_8=$(for b in $lib_base_7; do readelf -d ./.junest/usr/lib/"$b" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+	lib_base_8=$(echo "$lib_base_8" | tr ' ' '\n' | sort -u | xargs)
+	lib_base_9=$(for b in $lib_base_8; do readelf -d ./.junest/usr/lib/"$b" 2>/dev/null | grep NEEDED | tr '[] ' '\n' | grep ".so"; done)
+	lib_base_9=$(echo "$lib_base_9" | tr ' ' '\n' | sort -u | xargs)
+	lib_base_libs="$lib_core $lib_base_1 $lib_base_2 $lib_base_3 $lib_base_4 $lib_base_5 $lib_base_6 $lib_base_7 $lib_base_8 $lib_base_9"
+	lib_base_libs=$(echo "$lib_base_libs" | tr ' ' '\n' | sort -u | sed 's/.so.*/.so/' | xargs)
+	for l in $lib_base_libs; do
+		rsync -av ./.junest/usr/lib/"$l"* AppDir/lib/ &
+	done
+	wait
+}
+
+# Save files in /usr/share
+_saveshare() {
+	echo "◆ Saving directories in /usr/share"
+	SHARESAVED="$SHARESAVED $APP $BIN"
+	for arg in $SHARESAVED; do
+		cp -r ./.junest/usr/share/*"$arg"* AppDir/share/
+ 	done
+}
+
+_savebins 2>/dev/null
+_savelibs 2>/dev/null
+_saveshare 2>/dev/null
+
+printf -- "\n-----------------------------------------------------------------------------\n ASSEMBLING THE APPIMAGE\n-----------------------------------------------------------------------------\n"
+
+_post_installation_processes
+
+##########################################################################################################################################################
+#	REMOVE BLOATWARES
+##########################################################################################################################################################
+
+_remove_more_bloatwares() {
+	for r in $BIN_REMOVED; do rm -Rf AppDir/bin/"$r"*; done
+	for r in $LIB_REMOVED; do rm -Rf AppDir/lib/"$r"*; done
+	for r in $PYTHON_REMOVED; do rm -Rf AppDir/lib/python*/"$r"*; done
+	for r in $SHARE_REMOVED; do rm -Rf AppDir/share/"$r"*; done
+	find AppDir/share/doc/* -not -iname "*$BIN*" -a -not -name "." -delete 2> /dev/null #REMOVE ALL DOCUMENTATION NOT RELATED TO THE APP
+	find AppDir/share/locale/*/*/* -not -iname "*$BIN*" -a -not -name "." -delete 2> /dev/null #REMOVE ALL ADDITIONAL LOCALE FILES
+	rm -Rf AppDir/share/man # AppImages are not ment to have man command
+}
+
+##########################################################################################################################################################
+#	CREATE THE APPIMAGE WITH URUNTIME
+##########################################################################################################################################################
+
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
 chmod +x ./uruntime2appimage
 _JUNEST_CMD -- ./uruntime2appimage
