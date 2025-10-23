f2cb73a8dbfb04c26e46cf0abbd57d9a54c25247
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-10-23 03:34:51 +0200

    Set TAG to "latest"

--- NEW-junest.sh
+++ NEW-junest.sh
@@ -492,7 +492,7 @@ if test -f ./*.AppImage; then rm -Rf ./*archimage*.AppImage; fi
 
 APPNAME=$(cat AppDir/*.desktop | grep '^Name=' | head -1 | cut -c 6- | sed 's/ /-/g')
 REPO="$APPNAME-appimage"
-TAG="continuous"
+TAG="latest"
 VERSION="$VERSION"
 UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|$REPO|$TAG|*x86_64.AppImage.zsync"
 
