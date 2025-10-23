d0e5bebf68d9ff08c387169d3fe663caf22b737c
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-10-23 04:08:05 +0200

    Update NEW-junest.sh

--- NEW-junest.sh
+++ NEW-junest.sh
@@ -493,8 +493,9 @@ if test -f ./*.AppImage; then rm -Rf ./*archimage*.AppImage; fi
 APPNAME=$(cat AppDir/*.desktop | grep '^Name=' | head -1 | cut -c 6- | sed 's/ /-/g')
 REPO="$APPNAME-appimage"
 TAG="latest"
-VERSION="$VERSION"
 UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|$REPO|$TAG|*x86_64.AppImage.zsync"
+
+echo "$VERSION" > ./version
 
 _appimagetool() {
 	if ! command -v appimagetool 1>/dev/null; then
