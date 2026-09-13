d2ad3ffa269aacc70f90093baf3d8f112b1871b0
iVAN <88724353+ivan-hc@users.noreply.github.com>
2026-09-13 15:04:48 +0200

    Wait curl command on mirrorlist

--- core/archimage-builder.sh
+++ core/archimage-builder.sh
@@ -63,6 +63,7 @@ _junest_setup() {
 			TAKES_COUNT=0
 			while [ "$TAKES_COUNT" -lt 10 ]; do
 				MIRRORLIST=$(curl -Ls https://archlinux.org/mirrorlist/all | awk NR==2 RS=)
+				wait
 				if [ -z "$MIRRORLIST" ]; then
 					printf "\n The mirrorlist is empty, attempt %b of 10 will start in 5 seconds...\n\n" "$((TAKES_COUNT + 1))"
 					sleep 5
@@ -73,6 +74,7 @@ _junest_setup() {
 			TAKES_COUNT=0
 			while [ "$TAKES_COUNT" -lt 10 ]; do
 				MIRRORLIST=$(curl -Ls "https://archlinux.org/mirrorlist/?country=$COUNTRY")
+				wait
 				if [ -z "$MIRRORLIST" ]; then
 					printf "\n The mirrorlist is empty, attempt %b of 10 will start in 5 seconds...\n\n" "$((TAKES_COUNT + 1))"
 					sleep 5
